97d0cda8526577b19fcbc6131c30d09cc689058f
cvancory1 <cvancory1@gmail.com>
2021-10-31 18:29:36 -0400

    update timing

--- timingGJ.c
+++ timingGJ.c
@@ -42,16 +42,33 @@ int main(int argc, char** argv) {
   B.cols = 1;
 
   if(rank ==ROOT){
+      A.data = malloc(A.rows * A.cols * sizeof(double));
+      B.data = malloc(B.rows * B.cols * sizeof(double));
+    srand(time(0));
     for(int i =0; i <A.rows*A.cols;i++){
         A.data[i] = rand() % 100;
+        // printf("A.data[i=%f", A.data[i]);
     }
 
      for(int i =0; i <B.rows*B.cols;i++){
         B.data[i] = rand() % 100;
+        // printf("B.data[i=%f", B.data[i]);
+
     }
+  }else{
+      A.data = NULL;
+      B.data =NULL;
+
   }
 
-  gauss_j(A,B);
+//   gauss_j(A,B);
+
+    double t1 = MPI_Wtime();
+    gauss_j(A,B);
+    double t2 = MPI_Wtime();
+    if (rank == 0) {
+     printf("Gauss-Jordan Elimination for a %d by %d matrix took %f seconds\n", A.rows, A.cols, t2 - t1);
+    }
   
 
 
